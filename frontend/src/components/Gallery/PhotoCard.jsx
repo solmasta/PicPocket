8e39f4b584088b95e8c0836aacc0f9f80b6d243d
solmasta <lukedorsett@hotmail.com>
2026-08-19 22:54:31 -0500

    Fix typo in PhotoCard import

--- frontend/src/components/Gallery/PhotoCard.jsx
+++ frontend/src/components/Gallery/PhotoCard.jsx
@@ -1,5 +1,5 @@
 import { useState, memo } from 'react';
-import { useTranslation } from 'react-i18invoke';
+import { useTranslation } from 'react-i18next';
 import { usePhotos } from '../../hooks/usePhotos';
 import './PhotoCard.css';
 
